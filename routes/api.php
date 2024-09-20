8c1747bb63b134dbab8040833566bd431e17b02e
Er.Anuj <142636552+anuj1145@users.noreply.github.com>
2024-09-20 16:55:09 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,10 @@ use App\Http\Controllers\ActivityController;
 // Public Routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout']);
 
 // Protected Routes
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
     // Fetch logged-in user's activities by date range
     Route::get('/activities/date-range', [ActivityController::class, 'getActivitiesByDateRange']);
 });
